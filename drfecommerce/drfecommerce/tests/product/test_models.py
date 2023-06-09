25c7f429481deeaf3677a52162d9afe638facd1e
escandellari <elisa.scandellari@gmail.com>
2023-06-09 08:39:34 +0100

    Added unit test for 100% converage

--- drfecommerce/drfecommerce/tests/product/test_models.py
+++ drfecommerce/drfecommerce/tests/product/test_models.py
@@ -1,16 +1,24 @@
+import pytest
+
+pytestmark = pytest.mark.django_db
+
+
 class TestCategoryModel:
-    def test_str_method(self):
-        # Arrenge
+    def test_str_method(self, category_factory):
+        # Arrange
         # Act
+        category = category_factory(name="test category")
         # Assert
-        pass
+        assert category.__str__() == "test category"
 
 
 class TestBrandModel:
-    def test_str_method(self):
-        pass
+    def test_str_method(self, brand_factory):
+        brand = brand_factory(name="test brand")
+        assert brand.__str__() == "test brand"
 
 
 class TestProductModel:
-    def test_str_method(self):
-        pass
+    def test_str_method(self, product_factory):
+        brand = product_factory(name="test product")
+        assert brand.__str__() == "test product"
